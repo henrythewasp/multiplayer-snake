a4a43dfa06c78f21ea985961a47917ea2bb95c88
Gary Taylor <henrythewasp@gmail.com>
2021-01-04 13:05:22 +0000

    Add more checks to updatesnake

--- gamestate.go
+++ gamestate.go
@@ -23,17 +23,42 @@ type JSONSnakeData struct {
 type GameState struct {
 	mutex sync.RWMutex
 	snakes map[string]Snake
+	food []Pos
 }
 
 func NewGameState() *GameState {
+	log.Println("Building new GameState")
 	return &GameState{
 		snakes: make(map[string]Snake),
+		food: []Pos{NewRandomPos(false)},
 	}
 }
-func NewSnake() Snake {
+func (gs *GameState) NewSnake() Snake {
+	log.Println("Building new Snake")
 	s := make([]Pos, 0, 10)
-	s = append(s, Pos{rand.Intn(20), rand.Intn(20)})
+	s = append(s, gs.NewRandomFreePos(true))
 	return s
+}
+// will also need to check that this is a feee point on the grid and isn't
+// already taken up by a snake or food.
+func (gs *GameState) NewRandomFreePos(awayFromEdge bool) Pos {
+	var p Pos
+	for {
+		p = NewRandomPos(awayFromEdge)
+		if !gs.IsPosOccupied(p) {
+			break
+		}
+		log.Println("Pos is occupied. Try another.")
+	}
+	return p
+}
+func NewRandomPos(awayFromEdge bool) Pos {
+	if awayFromEdge {
+		// Return a position that is not too near the edge
+		return Pos{rand.Intn(12)+4, rand.Intn(12)+4}
+	} else {
+		return Pos{rand.Intn(20), rand.Intn(20)}
+	}
 }
 
 type JSONGameStateMsg struct {
@@ -44,14 +69,72 @@ type JSONGameStateMsg struct {
 func (gs *GameState) AddSnake(msg ClientMessage) string {
 	gs.mutex.Lock()
 	id := strconv.Itoa(len(gs.snakes)+1)
-	gs.snakes[id] = NewSnake()
+	gs.snakes[id] = gs.NewSnake()
 	gs.mutex.Unlock()
 	return id
 }
 func (gs *GameState) UpdateSnake(msg ClientMessage) {
+	isDead := false
+
+	s := msg.S
+	h := s[0]
+
 	gs.mutex.Lock()
-	gs.snakes[msg.Id] = msg.S
+
+	// Check if any bounds are exceeded by head
+	if h.X < 0 || h.X > 20 || h.Y < 0 || h.Y > 20 {
+		// Snake is DEAD  XXX TODO XXX
+		isDead = true
+	}
+
+	// Check if head is touching anything other than food
+	for _, v := range gs.snakes {
+		if IsPosInSlice(h, v) {
+			// Sname is DEAD XXX TODO XXX
+			isDead = true
+			break
+		}
+	}
+
+	if !gs.IsPosFood(h) {
+		// Remove the last element (moving, not growing)
+		s = s[1:]
+	}
+
+	if !isDead {
+		gs.snakes[msg.Id] = s
+	}
 	gs.mutex.Unlock()
+}
+
+// Need func to test if pos is food, if pos is occupied by another snake
+// (except THIS sname's head)
+func (gs *GameState) IsPosOccupied(p Pos) bool {
+	if gs.IsPosFood(p) {
+		return true
+	}
+
+	for _, v := range gs.snakes {
+		if IsPosInSlice(p, v) {
+			return true
+		}
+	}
+	return false
+}
+func (gs *GameState) IsPosFood(p Pos) bool {
+	return IsPosInSlice(p, gs.food)
+}
+
+func IsPosInSlice(p Pos, s []Pos) bool {
+	for _, pv := range s {
+		if IsEqualPos(p, pv) {
+			return true
+		}
+	}
+	return false
+}
+func IsEqualPos(p1 Pos, p2 Pos) bool {
+	return (p1.X == p2.X && p1.Y == p2.Y)
 }
 
 // This function needs fixing. It's not marshalling the payload properly.
